55916be50a667825daef6746b5dac30cc1b05651
Yan Couto <yancouto@gmail.com>
2021-03-06 17:02:52 +0000

    Tokenizer and some tests

--- src/interpreter/test.ts
+++ src/interpreter/test.ts
@@ -1,10 +1,34 @@
 
-let all_ok = true;
-if (all_ok) {
-    print("All tests passed! :)")
+import { tokenize, TokenType } from "./tokenizer";
 
+function nullthrows<T>(x: T | void): T {
+    if (x != null) {
+        return x;
+    } else {
+        throw new Error("null!");
+    }
+}
+
+function tokenizer_test() {
+    const iter = tokenize("a: b (c:de)");
+    assert(nullthrows(iter.next().value).value == "a");
+    assert(nullthrows(iter.next().value).type == TokenType.SYMBOL);
+    assert(nullthrows(iter.next().value).type == TokenType.TEXT);
+    assert(nullthrows(iter.next().value).value == "(");
+    assert(nullthrows(iter.next().value).value == "c");
+    assert(nullthrows(iter.next().value).value == ":");
+    assert(nullthrows(iter.next().value).value == "de");
+    assert(nullthrows(iter.next().value).type == TokenType.SYMBOL);
+    assert(iter.next().done);
+}
+
+const tests = [tokenizer_test];
+const failed = tests.map(test => pcall(test)).filter(result => result[0] == false).pop();
+if (failed == null) {
+    print("All tests passed! :)")
 } else {
     print("Some tests failed :(");
+    print(failed[1]);
     os.exit(1);
 }
 
