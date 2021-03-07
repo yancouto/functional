9614a94dc0b0e342c7b85532a341ece00b0b1aa3
Yan Couto <yancouto@gmail.com>
2021-03-07 19:59:38 +0000

    cursor blink

--- src/gamestates/editor.rs
+++ src/gamestates/editor.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use super::base::{GameState, GameStateEvent};
 use bracket_lib::prelude as bl;
 
@@ -5,6 +7,8 @@ use bracket_lib::prelude as bl;
 pub struct EditorState {
     text: Vec<Vec<char>>,
     cursor: (u8, u8),
+    cursor_blink_rate: Duration,
+    time: Duration,
 }
 
 fn with_current_console<F>(ctx: &mut bl::BTerm, f: F)
@@ -19,22 +23,28 @@ impl EditorState {
         Self {
             text: vec![vec![' '; 8]; 8],
             cursor: (0, 0),
+            cursor_blink_rate: Duration::from_secs_f32(0.5),
+            time: Duration::from_secs(0),
         }
     }
 
     fn print(&mut self, mut ctx: &mut bl::BTerm) {
         self.text[0][0] = 'x';
         self.text[1][1] = 'y';
+        self.time += Duration::from_secs_f32(ctx.frame_time_ms / 1000.);
+        let cursor_on = ((self.time.as_millis() / self.cursor_blink_rate.as_millis()) % 2) == 0;
         with_current_console(&mut ctx, |c| {
             self.text
                 .iter()
                 .enumerate()
                 .for_each(|(i, line)| c.print(0, i as i32, &line.iter().collect::<String>()));
-            c.set_bg(
-                self.cursor.0.into(),
-                self.cursor.1.into(),
-                bl::RGBA::from_f32(1., 1., 1., 0.5),
-            );
+            if cursor_on {
+                c.set_bg(
+                    self.cursor.0.into(),
+                    self.cursor.1.into(),
+                    bl::RGBA::from_f32(1., 1., 1., 0.5),
+                );
+            }
         });
     }
 }
