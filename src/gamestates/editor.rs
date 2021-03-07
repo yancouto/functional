1d7849353dc49fae703f8b4b2d599f91e24520cb
Yan Couto <yancouto@gmail.com>
2021-03-07 02:03:53 +0000

    Very simple text box

--- src/gamestates/editor.rs
+++ src/gamestates/editor.rs
@@ -2,11 +2,36 @@ use super::base::{GameState, GameStateEvent};
 use bracket_lib::prelude as bl;
 
 #[derive(Debug)]
-pub struct EditorState {}
+pub struct EditorState {
+    text: Vec<Vec<u8>>,
+    cursor: (u8, u8),
+}
+
+fn with_current_console<F>(ctx: &mut bl::BTerm, f: F)
+where
+    F: Fn(&mut Box<dyn bl::Console>) -> (),
+{
+    f(&mut bl::BACKEND_INTERNAL.lock().consoles[ctx.active_console].console);
+}
 
 impl EditorState {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            text: vec![vec![' ' as u8; 8]; 8],
+            cursor: (0, 0),
+        }
+    }
+
+    fn print(&mut self, mut ctx: &mut bl::BTerm) {
+        let mut builder = bl::TextBuilder::empty();
+        self.text[1][1] = 'x' as u8;
+        self.text.iter().for_each(|line| {
+            builder.append(&String::from_utf8_lossy(line));
+        });
+        builder.reset();
+        let mut block = bl::TextBlock::new(10, 10, 8, 8);
+        block.print(&builder).unwrap();
+        with_current_console(&mut ctx, |c| block.render(c));
     }
 }
 
@@ -16,7 +41,7 @@ impl GameState for EditorState {
     }
 
     fn tick(&mut self, ctx: &mut bl::BTerm) -> GameStateEvent {
-        ctx.print(10, 10, "hello world");
+        self.print(ctx);
         GameStateEvent::None
     }
 }
