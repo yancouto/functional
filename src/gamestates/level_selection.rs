189eb1bf4e6cf3a2bb1e523fd203164664761476
Yan Couto <yancouto@gmail.com>
2021-03-14 18:59:41 +0000

    level menu

--- src/gamestates/level_selection.rs
+++ src/gamestates/level_selection.rs
@@ -12,8 +12,11 @@ struct Section {
 
 #[derive(Debug)]
 pub struct LevelSelectionState {
-    section_cur: usize,
+    /// Index of selected section
+    section_i: usize,
     sections: Vec<Section>,
+    /// Index of selected level inside section
+    level_i: Option<usize>,
 }
 
 impl LevelSelectionState {
@@ -23,8 +26,8 @@ impl LevelSelectionState {
                 .map(|i| Level(format!("level {}", i)))
                 .collect::<Vec<Level>>()
         };
-        LevelSelectionState {
-            section_cur: 0,
+        let l = LevelSelectionState {
+            section_i: 0,
             sections: vec![
                 Section {
                     name: "basic".to_string(),
@@ -43,13 +46,21 @@ impl LevelSelectionState {
                     levels: random_levels(10),
                 },
             ],
+            level_i: None,
+        };
+        for section in &l.sections {
+            if section.name.len() as i32 + CURSOR_I + 2 > MID_I {
+                panic!("Too long name");
+            }
         }
+        l
     }
 }
 
 const CURSOR_I: i32 = 3;
 const START_J: i32 = 2;
 const LINES_PER_SECTION: i32 = 3;
+const MID_I: i32 = 40;
 
 impl GameState for LevelSelectionState {
     fn name(&self) -> &'static str {
@@ -65,10 +76,25 @@ impl GameState for LevelSelectionState {
             );
         }
         let cursor_on = ((data.time.as_millis() / 500) % 2) == 0;
+        if let Some(l_i) = self.level_i {
+            for (i, level) in self.sections[self.section_i].levels.iter().enumerate() {
+                data.console.print(
+                    MID_I + CURSOR_I + 2,
+                    START_J + LINES_PER_SECTION * i as i32,
+                    &level.0,
+                );
+            }
+        } else {
+            data.console.print(
+                MID_I + CURSOR_I,
+                START_J + LINES_PER_SECTION * self.section_i as i32,
+                "press RIGHT to open section",
+            );
+        }
         if cursor_on {
             data.console.print(
-                CURSOR_I,
-                START_J + LINES_PER_SECTION * self.section_cur as i32,
+                CURSOR_I + self.level_i.map_or(0, |_| MID_I),
+                START_J + LINES_PER_SECTION * self.level_i.unwrap_or(self.section_i) as i32,
                 ">",
             );
         }
@@ -78,20 +104,32 @@ impl GameState for LevelSelectionState {
     fn on_event(&mut self, event: bl::BEvent) -> () {
         match event {
             bl::BEvent::KeyboardInput {
-                key: bl::VirtualKeyCode::Down,
-                pressed: true,
-                ..
-            } => {
-                self.section_cur = (self.section_cur + 1) % self.sections.len();
-            }
-            bl::BEvent::KeyboardInput {
-                key: bl::VirtualKeyCode::Up,
-                pressed: true,
-                ..
-            } => {
-                self.section_cur =
-                    (self.section_cur + self.sections.len() - 1) % self.sections.len();
-            }
+                key, pressed: true, ..
+            } => match key {
+                bl::VirtualKeyCode::Down => {
+                    if let Some(li) = self.level_i {
+                        self.level_i = Some((li + 1) % self.sections[self.section_i].levels.len());
+                    } else {
+                        self.section_i = (self.section_i + 1) % self.sections.len();
+                    }
+                }
+                bl::VirtualKeyCode::Up => {
+                    if let Some(li) = self.level_i {
+                        let len = self.sections[self.section_i].levels.len();
+                        self.level_i = Some((li + len - 1) % len);
+                    } else {
+                        self.section_i =
+                            (self.section_i + self.sections.len() - 1) % self.sections.len();
+                    }
+                }
+                bl::VirtualKeyCode::Right if self.level_i.is_none() => {
+                    self.level_i = Some(0);
+                }
+                bl::VirtualKeyCode::Left if self.level_i.is_some() => {
+                    self.level_i = None;
+                }
+                _ => {}
+            },
             _ => {}
         }
     }
