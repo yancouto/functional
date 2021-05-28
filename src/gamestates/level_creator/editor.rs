f14f0dd1b8355e84aa3f5d588b46a7b34b027250
Yan Couto <yancouto@gmail.com>
2021-05-28 15:36:55 +0100

    Use YAML instead of binary format

--- src/gamestates/level_creator/editor.rs
+++ src/gamestates/level_creator/editor.rs
@@ -1,11 +1,12 @@
 use std::path::PathBuf;
 
+use serde::{Deserialize, Serialize};
+
 use super::super::base::*;
 use crate::prelude::*;
-const WORKSHOP_CONFIG_VERSION: u32 = 0;
-const WORKSHOP_FILE: &str = "workshop.data";
 
-#[derive(Savefile, Default)]
+const WORKSHOP_FILE: &str = "workshop.yaml";
+#[derive(Debug, Default, Serialize, Deserialize)]
 struct WorkshopConfig {
     published_id: Option<u64>,
 }
@@ -18,20 +19,28 @@ pub struct EditorState {
 impl EditorState {
     pub fn new(root: PathBuf) -> Self { Self { root } }
 
+    fn workshop_file(&self) -> PathBuf { self.root.join(WORKSHOP_FILE) }
+
     fn read_config(&self) -> WorkshopConfig {
-        self.root
-            .join(WORKSHOP_FILE)
-            .to_str()
-            .and_then(|filename| savefile::load_file(filename, WORKSHOP_CONFIG_VERSION).ok())
-            .debug_unwrap_or(WorkshopConfig::default())
+        match std::fs::File::open(self.workshop_file()).map(|f| serde_yaml::from_reader(f)) {
+            Ok(Ok(config)) => config,
+            err @ _ => {
+                log::warn!("Failed to read workshop config! {:?}", err);
+                debug_assert!(false);
+                WorkshopConfig::default()
+            },
+        }
     }
 
     fn write_config(&self, config: &WorkshopConfig) {
-        self.root
-            .join(WORKSHOP_FILE)
-            .to_str()
-            .and_then(|name| savefile::save_file(name, WORKSHOP_CONFIG_VERSION, config).ok())
-            .debug_unwrap();
+        match std::fs::File::create(self.workshop_file()).map(|f| serde_yaml::to_writer(f, config))
+        {
+            Ok(Ok(_)) => {},
+            err @ _ => {
+                log::warn!("Failed to write workshop config! {:?}", err);
+                debug_assert!(false);
+            },
+        }
     }
 }
 
